d8146236b3380e2ec6e72b86d62272a661641da9
Kurt Manninen <kurt@cranksetanalytics.com>
2021-10-22 12:39:55 -0700

    Added get_documentsTop for consisten training data.

--- demo/api/db.py
+++ demo/api/db.py
@@ -13,6 +13,10 @@ class DB_MOVIES:
         self.cursor.execute("SELECT title, plot FROM wiki_movie_plots_deduped ORDER BY rand() limit {:d}".format(amount))
         return self.cursor.fetchall()
     
+    def get_documentsTop(self, amount):
+        self.cursor.execute("SELECT title, plot FROM wiki_movie_plots_deduped ORDER BY title limit {:d}".format(amount))
+        return self.cursor.fetchall()
+    
     def get_topic(self, id):
         self.cursor.execute("SELECT topic_text, topic_name FROM movie_topics WHERE id = {:d}".format(id))
         return self.cursor.fetchall()
